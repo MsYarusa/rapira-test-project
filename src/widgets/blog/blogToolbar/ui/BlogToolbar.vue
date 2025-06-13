f0691577fe9f6b7dd8c06acbedab0a68755b5934
MsYarusa <zaporozhskii.yl@dvfu.ru>
2025-06-13 23:45:51 +1000

    added blog toolbar

--- src/widgets/blog/blogToolbar/ui/BlogToolbar.vue
+++ src/widgets/blog/blogToolbar/ui/BlogToolbar.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="bg-white">
-    <h1>{{ t('blog_page_title') }}</h1>
-    <BaseSearchInput v-model="searchValue" />
+  <div class="bg-white px-[10px] sm:px-[30px] lg:px-[95px]">
+    <div class="flex flex-col gap-[10px] py-3 sm:py-5">
+      <div class="flex justify-between gap-4">
+        <div class="flex w-full items-center gap-[40px]">
+          <h1 class="text-2xl font-bold sm:text-[32px]">
+            {{ t('blog_page_title') }}
+          </h1>
+
+          <BaseSearchInput
+            v-model="searchValue"
+            class="max-sm:hidden!"
+          />
+        </div>
+
+        <FilterVisibilityButtons
+          :filter-is-empty="filteredCategories.length === 0"
+        />
+      </div>
+
+      <BaseSearchInput
+        v-model="searchValue"
+        class="max-w-full! sm:hidden!"
+      />
+    </div>
   </div>
 </template>
 
@@ -10,6 +31,12 @@ import { ref } from 'vue'
 
 import { useI18nModule } from '@/shared/lib/i18n'
 import { BaseSearchInput } from '@/shared/ui/components'
+
+import type { Category } from '@/entities/blog/categories'
+
+import FilterVisibilityButtons from './FilterVisibilityButtons.vue'
+
+const filteredCategories = ref<Category[]>([])
 
 const { t } = useI18nModule({
   messagesObject: { ru: { blog_page_title: 'Блог' } },
--- src/widgets/blog/blogToolbar/ui/FilterVisibilityButtons.vue
+++ src/widgets/blog/blogToolbar/ui/FilterVisibilityButtons.vue
@@ -1,7 +1,57 @@
 <template>
-  <div></div>
+  <div class="flex min-w-fit gap-[10px] text-sm">
+    <button
+      v-if="filterIsActive && !filterIsEmpty"
+      class="text-primary hover:text-primary-active cursor-pointer"
+      @click="emit('clear')"
+    >
+      {{ t('common.clear_filter_btn') }}
+    </button>
+
+    <button
+      class="group flex cursor-pointer items-center gap-1 text-gray-400 hover:text-gray-500"
+      @click="toggleActive"
+    >
+      <span v-if="filterIsActive">{{ t('common.filter_active_btn') }}</span>
+
+      <span v-else>{{ t('common.filter_btn') }}</span>
+
+      <ArrowUpIcon
+        v-if="filterIsActive"
+        class="size-[16px] fill-gray-400 group-hover:fill-gray-500"
+      />
+      <ArrowDownIcon
+        v-else
+        class="size-[16px] fill-gray-400 group-hover:fill-gray-500"
+      />
+    </button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+import { ArrowDownIcon, ArrowUpIcon } from '@/shared/ui/assets/icons'
+
+interface FilterVisibilityButtonsProps {
+  filterIsEmpty: boolean
+}
+
+defineProps<FilterVisibilityButtonsProps>()
+
+interface FilterVisibilityButtonsEmits {
+  (event: 'clear'): void
+}
+
+const emit = defineEmits<FilterVisibilityButtonsEmits>()
+
+const filterIsActive = defineModel<boolean>('active')
+
+const toggleActive = (): void => {
+  filterIsActive.value = !filterIsActive.value
+}
+
+const { t } = useI18n()
+</script>
 
 <style scoped></style>
