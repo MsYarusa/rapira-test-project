0e623a954a6b57bd725757918e296bf31418034e
MsYarusa <zaporozhskii.yl@dvfu.ru>
2025-06-15 22:00:32 +1000

    added page container to home page

--- src/pages/home/ui/HomeView.vue
+++ src/pages/home/ui/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>{{ t('home_page_title') }}</div>
+  <PageContainer>
+    <div>{{ t('home_page_title') }}</div>
+  </PageContainer>
 </template>
 
 <script lang="ts" setup>
 import { useI18nModule } from '@/shared/lib/i18n'
+import { PageContainer } from '@/shared/ui/components'
 
 import { messagesObject } from '../locale'
 
