dad0debbd9920f299476e75fefffdcd2c3ebb232
MsYarusa <zaporozhskii.yl@dvfu.ru>
2025-06-15 12:45:46 +1000

    added loader skeleton

---
+++ src/shared/ui/components/Loader/LoaderSkeleton/LoaderSkeleton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="loading-skeleton__container">
+    <div class="loading-skeleton"></div>
+  </div>
+</template>
+
+<style lang="scss">
+@import './LoaderSkeleton.scss';
+</style>
--- src/shared/ui/components/index.ts
+++ src/shared/ui/components/index.ts
@@ -1,6 +1,8 @@
 // Form controls
 import BaseSearchInput from './FormControls/BaseSearchInput.vue'
+// Loaders
+import LoaderSkeleton from './Loader/LoaderSkeleton/LoaderSkeleton.vue'
 // Modals
 import ModalBackground from './Modal/ModalBackground.vue'
 
-export { ModalBackground, BaseSearchInput }
+export { ModalBackground, BaseSearchInput, LoaderSkeleton }
