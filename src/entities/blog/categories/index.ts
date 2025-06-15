f6250039111144d6c5420fb65fa09de6df7cccd6
MsYarusa <zaporozhskii.yl@dvfu.ru>
2025-06-15 16:05:35 +1000

    added category tag component

--- src/entities/blog/categories/index.ts
+++ src/entities/blog/categories/index.ts
@@ -1,3 +1,6 @@
+import CategoryTag from './ui/CategoryTag.vue'
+
+export { CategoryTag }
 export { useCategoriesStore } from './model/CategoriesStore'
 export type { Category } from './model/Category'
 export { mapCategoryApiToCategory } from './lib/mappers/apiMappers'
---
+++ src/entities/blog/categories/ui/CategoryTag.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="bg-primary-light text-primary rounded-[20px] px-[14px] py-[6px] text-sm leading-0"
+  >
+    {{ category.title }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Category } from '../model/Category'
+
+interface CategoryTagProps {
+  category: Category
+}
+
+defineProps<CategoryTagProps>()
+</script>
