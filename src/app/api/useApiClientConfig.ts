c52a83df87860ed926c2b74c568bf46d691127c8
MsYarusa <zaporozhskii.yl@dvfu.ru>
2025-06-17 15:26:35 +1000

    added documentation

--- src/app/api/useApiClientConfig.ts
+++ src/app/api/useApiClientConfig.ts
@@ -1,7 +1,9 @@
 import { client } from '@/shared/api/openapi/client/client.gen'
 
+import { API_BASE_URL_DEFAULT } from './config'
+
 export function useApiClientConfig(): void {
   client.setConfig({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || API_BASE_URL_DEFAULT,
   })
 }
